019a1c078b8c5edc2878744a32b99f359bcb957e
Sudipan Saha <119653610+sudipan-saha@users.noreply.github.com>
2022-12-02 12:26:28 +0530

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+
+    const[count,setCount]=useState(0)
+    const handleIncreament=()=> 
+    {
+        setCount(count+1)
+    }
+
+    const handleDecreament=()=> 
+    {
+        setCount(count-1)
+    }
+
+    const reset=()=>
+    {
+        setCount(0)
+    }
+
+    return (
+        <div className="counter center">
+            <h1 className="counter__title">Counter App</h1>
+            <div className="card center">
+                <h2 className="card__title">Count : {count}</h2>
+            <div className="card__btns">
+                <button disabled={(count>=5?true:false)} onClick={handleIncreament} className="btn card__btn">+</button>
+                <button disabled={(count<=-5?true:false)}onClick={handleDecreament} className="btn card__btn">-</button>
+                <button onClick={reset} className="btn card__btn">0</button>
+            </div>
+            </div>
+            </div>
+    );
+};
+
+export default Counter;
+
+
